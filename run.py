2acbb0e0d2e4484917960430c8209a1389ed8214
jona-tech <jonathanbernhardsson1@gmail.com>
2022-12-18 15:29:30 +0000

    add functions to place the ships on random locations and create loop to print the board with iterating row numbers starting on 1

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 from random import randint
 
-#create the board and ships list
+# Create the board and ships list
 board = [[' '] * 5 for x in range(5)]
 ships = [[' '] * 5 for x in range(5)]
+
+
+# Place the ships on the board at random locations
+for i in range(5):
+    r = randint(0, 4)
+    c = randint(0, 4)
+    ships[r][c] = 'S'
+
+
+# Function to print the board
+rows = 1
+for row in board:
+    print(rows, '|'.join(row))
+    rows += 1
+
